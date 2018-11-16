93bf8f98ad0b34e3f0b69e8b39341a82551b77f8
tech-a08 <you@example.com>
2018-11-16 20:09:36 +0000

    gg

---
+++ modules/app_tier/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_id" {
+  default = "vpc-02ee46f22955a5b81"
+}
+
+variable "ig_id" {
+  default = "igw-08bc9b3838ff3ddf3"
+}
+
+variable "ami_id" {
+  default = "ami-055c1755b888344f7"
+}
+
+variable "user_data" {
+  description = "user data"
+  type        = "string"
+  default     = ""
+}
